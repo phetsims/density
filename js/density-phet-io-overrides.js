b92ca93a60b2c1cd0899150d3ad2e4ca89d1b718
Amy Rouinfar <amy.rouinfar@colorado.edu>
2021-10-19 20:34:58 -0600

    phetioFeatured overrides for Intro screen https://github.com/phetsims/density/issues/98

--- js/density-phet-io-overrides.js
+++ js/density-phet-io-overrides.js
@@ -1,2 +1,94 @@
 /* eslint-disable */
-window.phet.preloads.phetio.phetioElementsOverrides = {};
+window.phet.preloads.phetio.phetioElementsOverrides =
+  {
+    "density.introScreen.model.blocks.blockA.inputEnabledProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.model.blocks.blockA.massProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.model.blocks.blockA.materialProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.model.blocks.blockA.userControlledProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.model.blocks.blockA.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.model.blocks.blockA.volumeProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.model.blocks.blockB.massProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.model.blocks.blockB.materialProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.model.blocks.blockB.userControlledProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.model.blocks.blockB.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.model.blocks.blockB.volumeProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.model.densityExpandedProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.model.pool.liquidVolumeProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.model.showMassesProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.view.blockAControlPanel.comboBoxMaterialProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.view.blockAControlPanel.massNumberControl.enabledMassRangeProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.view.blockAControlPanel.massNumberControl.massNumberProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.view.blockAControlPanel.massNumberControl.titleNode.textProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.view.blockAControlPanel.massNumberControl.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.view.blockAControlPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.view.blockAControlPanel.volumeNumberControl.titleNode.textProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.view.blockAControlPanel.volumeNumberControl.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.view.blockBControlPanel.comboBoxMaterialProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.view.blockBControlPanel.massNumberControl.enabledMassRangeProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.view.blockBControlPanel.massNumberControl.massNumberProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.view.blockBControlPanel.massNumberControl.titleNode.textProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.view.blockBControlPanel.massNumberControl.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.view.blockBControlPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.view.blockBControlPanel.volumeNumberControl.titleNode.textProperty": {
+      "phetioFeatured": true
+    },
+    "density.introScreen.view.blockBControlPanel.volumeNumberControl.visibleProperty": {
+      "phetioFeatured": true
+    }
+  };

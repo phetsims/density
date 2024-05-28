74ffb746b20451cd499879c4ab7eb353bfd07619
Sam Reid <reids@colorado.edu>
2024-05-28 15:25:35 -0600

    Move overrides into the code, see https://github.com/phetsims/density-buoyancy-common/issues/134

--- js/density-phet-io-overrides.js
+++ js/density-phet-io-overrides.js
@@ -6,14 +6,5 @@ window.phet.preloads.phetio.phetioElementsOverrides =
     },
     "density.introScreen.view.blockAControlPanel.massNumberControl.numberControlMassProperty": {
       "phetioFeatured": true
-    },
-    "density.mysteryScreen.model.scale.materialProperty": {
-      "phetioFeatured": false
-    },
-    "density.mysteryScreen.model.scale.volumeProperty": {
-      "phetioFeatured": false
-    },
-    "density.mysteryScreen.model.scale.inputEnabledProperty": {
-      "phetioFeatured": false
     }
   };

3a28698120da3c497ec8ac9759b348b0fb08d966
Sam Reid <reids@colorado.edu>
2024-05-28 15:34:03 -0600

    Move overrides into the code, see https://github.com/phetsims/density-buoyancy-common/issues/134

--- js/density-phet-io-overrides.js
+++ js/density-phet-io-overrides.js
@@ -1,9 +1,6 @@
 /* eslint-disable */
 window.phet.preloads.phetio.phetioElementsOverrides =
   {
-    "density.compareScreen.view.blocksPanel.blocksRadioButtonGroup.visibleProperty": {
-      "phetioFeatured": false
-    },
     "density.introScreen.view.blockAControlPanel.massNumberControl.numberControlMassProperty": {
       "phetioFeatured": true
     }
